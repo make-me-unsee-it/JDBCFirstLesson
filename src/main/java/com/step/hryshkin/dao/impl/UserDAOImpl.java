ec7f4394681c8550f281860d851bd80b8865d0f0
alekx <alekx-gr@yandex.by>
2022-06-30 21:14:23 +0300

    one more method

--- src/main/java/com/step/hryshkin/dao/impl/UserDAOImpl.java
+++ src/main/java/com/step/hryshkin/dao/impl/UserDAOImpl.java
@@ -9,6 +9,7 @@ import org.apache.logging.log4j.Logger;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Optional;
 
@@ -31,6 +32,20 @@ public class UserDAOImpl implements UserDAO {
 
     @Override
     public Optional<User> getUserByName(String userName) {
-        return Optional.empty();
+        Optional<User> result = Optional.empty();
+        try (Connection connection = Connector.createConnection()) {
+            try (PreparedStatement statement = connection
+                    .prepareStatement("SELECT * FROM USERS WHERE USERNAME = '" + userName + "'")) {
+                ResultSet rs = statement.executeQuery();
+                while (rs.next()) {
+                    result = Optional.of(new User(rs.getLong("ID"),
+                    rs.getString("USERNAME"),
+                    rs.getString("PASSWORD")));
+                }
+            }
+        } catch (SQLException exception) {
+            LOGGER.error("SQLException at UserDAOImpl at getUserByName(String username)");
+        }
+        return result;
     }
 }
